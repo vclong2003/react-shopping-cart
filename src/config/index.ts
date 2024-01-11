b3a18601e048b4db366feb557319288c3aeb30e5
vclong2003 <vclong2003@gmail.com>
2024-01-11 09:01:00 +0700

    Add axios dependency and update App component

---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export const API_ENDPOINT = "http://localhost:4000/api/products";
---
+++ src/features/products/interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface IProduct {
+  productId: string;
+  productName: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+}
