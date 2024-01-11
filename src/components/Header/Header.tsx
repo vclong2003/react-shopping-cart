7021fb7a5769af6480f0fd7962e778c4f561e447
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-11 13:17:41 +0700

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
-import { AppBar, Button, Stack, Toolbar } from "@mui/material";
+import { AppBar, Button, Stack, Tab, Tabs } from "@mui/material";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function Header() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Stack direction="row" spacing={2}>
-          <Button>Home</Button>
-          <Button>Products</Button>
-          <Button>Reviews</Button>
-        </Stack>
-      </Toolbar>
+    <AppBar color={"transparent"} position="static">
+      <Stack direction={"row"} spacing={2}>
+        <Tabs>
+          <Tab label="Home" value="/" to="/" component={Link} />
+          <Tab
+            label="Products"
+            value="/products"
+            to="/products"
+            component={Link}
+          />
+          <Tab
+            label="Reviews"
+            value="/reviews"
+            to="/reviews"
+            component={Link}
+          />
+        </Tabs>
+      </Stack>
     </AppBar>
   );
 }
