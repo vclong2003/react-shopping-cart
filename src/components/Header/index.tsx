175a70261df1d4c8d2c541d7e9eeda9b5f262c9b
vclong2003 <vclong2003@gmail.com>
2024-01-12 14:41:48 +0700

    Remove unused dependencies and components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,3 @@
-import { AppBar, Container, Grid, Toolbar } from "@mui/material";
-
-import Logo from "../Logo";
-import ResponsiveMenu from "./ResponsiveMenu";
-import Menu from "./Menu";
-
 export default function Header(): JSX.Element {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Grid container>
-          <Grid item xs={4}>
-            <ResponsiveMenu />
-            <Menu />
-          </Grid>
-          <Grid item xs={4}>
-            <Logo />
-          </Grid>
-          <Grid item xs={4}></Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  );
+  return <>Header</>;
 }
