bad93a87d87675a77fc4ca37627236e232fc46d6
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-01-12 16:30:01 +0700

    okok

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Layout from "../components/Layout";
 import Products from "../pages/Products";
+import Checkout from "../pages/Checkout/Checkout";
 
 export default function AppRoutes(): JSX.Element {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes(): JSX.Element {
       <Route path="/" element={<Layout />}>
         <Route index element={<h1>Home</h1>} />
         <Route path="products" element={<Products />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
       <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
