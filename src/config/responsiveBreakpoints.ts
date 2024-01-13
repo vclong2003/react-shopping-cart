a4aa1e24d6fd2a4346aec57d887dfe74c57e56cc
vclong2003 <vclong2003@gmail.com>
2024-01-13 16:02:28 +0700

    change breakpoints, fix Container

--- src/config/responsiveBreakpoints.ts
+++ src/config/responsiveBreakpoints.ts
@@ -1,19 +1,15 @@
 const SIZES = {
-  MOBILE_S: "320px",
-  MOBILE_M: "375px",
-  MOBILE_L: "425px",
-  TABLET: "768px",
-  LAPTOP: "1024px",
-  LAPTOP_L: "1440px",
-  DESKTOP: "2560px",
+  PHONE: "576px",
+  PHONE_L: "768px",
+  TABLET: "992px",
+  DESKTOP: "1200px",
+  DESKTOP_L: "1400px",
 };
 
 export const DEVICES = {
-  MOBILE_S: `(min-width: ${SIZES.MOBILE_S})`,
-  MOBILE_M: `(min-width: ${SIZES.MOBILE_M})`,
-  MOBILE_L: `(min-width: ${SIZES.MOBILE_L})`,
+  PHONE: `(min-width: ${SIZES.PHONE})`,
+  PHONE_L: `(min-width: ${SIZES.PHONE_L})`,
   TABLET: `(min-width: ${SIZES.TABLET})`,
-  LAPTOP: `(min-width: ${SIZES.LAPTOP})`,
-  LAPTOP_L: `(min-width: ${SIZES.LAPTOP_L})`,
   DESKTOP: `(min-width: ${SIZES.DESKTOP})`,
+  DESKTOP_L: `(min-width: ${SIZES.DESKTOP_L})`,
 };
