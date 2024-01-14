fe894998f7d392922dbf5d0771c4054081c02888
vclong2003 <vclong2003@gmail.com>
2024-01-14 15:53:30 +0700

    Refactor ProductDetail component

--- src/pages/Products/ProductDetail/ProductDetail.tsx
+++ src/pages/Products/ProductDetail/ProductDetail.tsx
@@ -3,20 +3,27 @@ import * as S from "./ProductDetail.styled";
 import CartQuantityButton from "../CartQuantityButton/CartQuantityButton";
 import { useState } from "react";
 
-export default function ProductDetail({ product }: { product: IProduct }) {
+interface IProductDetailProps {
+  product: IProduct;
+}
+
+export default function ProductDetail({ product }: IProductDetailProps) {
   const [quantity, setQuantity] = useState<number>(1);
+
+  const { imageUrl, productName, description, price } = product;
+
   return (
     <S.ProductDetail>
       <S.ImageContainer>
-        <S.Image src="https://picsum.photos/700/500" alt="" />
+        <S.Image src={imageUrl} alt="" />
       </S.ImageContainer>
-      <S.Name>{product.productName}</S.Name>
-      <S.Description>{product.description}</S.Description>
+      <S.Name>{productName}</S.Name>
+      <S.Description>{description}</S.Description>
 
       <S.ButtonRow>
         <CartQuantityButton quantity={quantity} onSetQuantity={setQuantity} />
         <S.Splitter />
-        <S.Price>{product.price}$</S.Price>
+        <S.Price>{price}$</S.Price>
         <S.AddToCartBtn>
           <i className="bi bi-cart-fill"></i> Add to cart
         </S.AddToCartBtn>
