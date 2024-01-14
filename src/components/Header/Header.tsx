51294f5e24cc6ff458cfa0e8c1e953eb87e6f10d
vclong2003 <vclong2003@gmail.com>
2024-01-14 13:12:31 +0700

    Refactor header component and remove commented code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,21 +6,20 @@ import { useState } from "react";
 export default function Header(): JSX.Element {
   const [isOffcanavsVisible, setIsOffCanvasVisible] = useState<boolean>(false);
 
-  const handleOpenNavList = () => setIsOffCanvasVisible(true);
-  const handleCloseNavList = () => setIsOffCanvasVisible(false);
+  const openOffcanvas = () => setIsOffCanvasVisible(true);
+  const closeOffCanvas = () => setIsOffCanvasVisible(false);
 
   return (
     <S.Header>
       <S.Nav>
-        <S.ResponsiveNavButton onClick={handleOpenNavList}>
+        <S.ResponsiveNavButton onClick={openOffcanvas}>
           <i className="bi bi-list"></i>
         </S.ResponsiveNavButton>
-        <OffCanvas show={isOffcanavsVisible} onClose={handleCloseNavList}>
+        <OffCanvas show={isOffcanavsVisible} onClose={closeOffCanvas}>
           <S.VerticalNavList />
         </OffCanvas>
         <S.NavList />
         <S.Logo>Beauty.com</S.Logo>
-
         <S.CartButton to="/checkout">
           <Badge count={1}>
             <S.CartIcon>
