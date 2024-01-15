43e9c5dfd3469762b4a180fe7a49191c2cef009e
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-15 11:11:36 +0700

    fixes

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = (process.env.API_URL as string) || "http://localhost:3000";
+const API_BASE_URL =
+  (process.env.API_URL as string) ||
+  "https://vcl-shopping-cart-api.onrender.com/api";
 
 console.log("API_BASE_URL", API_BASE_URL);
 const API_ENDPOINTS = {
