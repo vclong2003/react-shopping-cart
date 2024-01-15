208698831a49cb33f3a63bc1537d6b24f871d78e
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-15 09:58:04 +0700

    ProductList

--- src/pages/Products/ProductList/ProductList.tsx
+++ src/pages/Products/ProductList/ProductList.tsx
@@ -1,31 +1,38 @@
 import { IProduct } from "../../../interfaces";
+import S from "./ProductList.styled";
 
 interface IProductListProp {
   products: Array<IProduct>;
+  setProductIndex: (index: number) => void;
 }
 
-export default function ProductList({ products }: IProductListProp) {
+export default function ProductList({
+  products,
+  setProductIndex,
+}: IProductListProp) {
   return (
-    <div className="ProductList">
+    <S.ProductList>
       {products.map((product, index) => {
         return (
-          <div className="ListItem" key={index}>
-            <div className="ImageContainer">
-              <img className="Image" src={product.imageUrl} alt="" />
-            </div>
-            <div className="Details">
-              <div style={{ width: "100%" }}>
-                <div className="Name">{product.productName}</div>
-                <div className="Description">{product.description}</div>
-                <div className="MoreDetails">
-                  <div className="Price">${product.price}</div>
-                  <div className="DetailsClickable">Details</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <S.ProductItem key={index}>
+            <S.ImageContainer>
+              <S.Image src={product.imageUrl} alt="" />
+            </S.ImageContainer>
+            <S.Details>
+              <S.Name>{product.productName}</S.Name>
+              <S.Description>{product.description}</S.Description>
+              <S.Splitter></S.Splitter>
+              <S.MoreDetails>
+                <S.Price>${product.price}</S.Price>
+                <S.Splitter></S.Splitter>
+                <S.MoreDetailsClickable onClick={() => setProductIndex(index)}>
+                  Details
+                </S.MoreDetailsClickable>
+              </S.MoreDetails>
+            </S.Details>
+          </S.ProductItem>
         );
       })}
-    </div>
+    </S.ProductList>
   );
 }
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAllProducts } from "../../store/slices/products";
 import ProductDetail from "./ProductDetail/ProductDetail";
 import ProductList from "./ProductList/ProductList";
@@ -10,6 +10,7 @@ export default function Products(): JSX.Element {
   const dispatch = useDispatch<AppDispatch>();
   const { products } = useSelector((state: RootState) => state.products);
 
+  const [productIndex, setProductIndex] = useState<number>(0);
   useEffect(() => {
     dispatch(fetchAllProducts());
   }, [dispatch]);
@@ -20,10 +21,21 @@ export default function Products(): JSX.Element {
     <S.ProductPage>
       <S.PageContent>
         <S.Left>
-          {products.length !== 0 ? <ProductDetail product={products[0]} /> : ""}
+          {products.length !== 0 ? (
+            <ProductDetail product={products[productIndex]} />
+          ) : (
+            ""
+          )}
         </S.Left>
         <S.Right>
-          {products.length !== 0 ? <ProductList products={products} /> : ""}
+          {products.length !== 0 ? (
+            <ProductList
+              setProductIndex={setProductIndex}
+              products={products}
+            />
+          ) : (
+            ""
+          )}
         </S.Right>
       </S.PageContent>
     </S.ProductPage>
