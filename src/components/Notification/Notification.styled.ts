65b4fb6801ace2ea384698f13ab21b8ee88769e9
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-01-16 14:20:54 +0700

    notifi message fix

--- src/components/Notification/Notification.styled.ts
+++ src/components/Notification/Notification.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICES } from "../../config/responsiveBreakpoints";
 
 interface INotificationContainerProps {
   $show?: boolean;
@@ -12,8 +13,19 @@ export const NotificationContainer = styled.div<INotificationContainerProps>`
   margin: var(--s-3);
   border-radius: var(--s-1);
   position: fixed;
-  bottom: var(--s-2);
-  right: var(--s-9);
+  z-index: 10;
+  @media ${DEVICES.PHONE} {
+    top: var(--s-2);
+    left: var(--s-2);
+    bottom: unset;
+    right: unset;
+  }
+  @media ${DEVICES.TABLET} {
+    bottom: var(--s-2);
+    right: var(--s-2);
+    top: unset;
+    left: unset;
+  }
 `;
 
 export const Message = styled.p``;
