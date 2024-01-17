918ec69fbac1638ad426a6fd29d9cb08cbcfc5d9
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-17 14:53:51 +0700

    Loader

--- src/routes/layoutRoutes.tsx
+++ src/routes/layoutRoutes.tsx
@@ -4,6 +4,7 @@ import Checkout from "../pages/Checkout/Checkout";
 import Home from "../pages/Home/Home";
 import Products from "../pages/Products/Products";
 import Review from "../pages/Reviews/Review";
+import PageLoader from "../components/PageLoader/PageLoader";
 
 export const layoutRoutes: IRoute[] = [
   {
@@ -19,4 +20,5 @@ export const layoutRoutes: IRoute[] = [
     component: <Review />,
   },
   { path: "/checkout", component: <Checkout /> },
+  { path: "/buh", component: <PageLoader /> },
 ];
