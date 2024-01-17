4a22e7195877a7141da30ef56090317ef05468d5
vclong2003 <vclong2003@gmail.com>
2024-01-17 16:29:39 +0700

    Update API_BASE_URL in config file

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,6 @@
 const API_BASE_URL =
-  (process.env.API_URL as string) || "http://localhost:4000/api";
+  (process.env.API_URL as string) ||
+  "https://vcl-shopping-cart-api.onrender.com/api";
 
 console.log("API_BASE_URL", API_BASE_URL);
 const API_ENDPOINTS = {
