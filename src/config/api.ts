4e430798127b2e6e80df79566fd3769180740467
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-17 17:56:54 +0700

    build prod (not done)

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 const API_BASE_URL =
   (process.env.API_URL as string) || "http://localhost:4000/api";
 
+console.log(process.env.API_URL);
 console.log("API_BASE_URL", API_BASE_URL);
 const API_ENDPOINTS = {
   PRODUCTS: "/products",
