e6f933e7dea6c6936cb6293107189a6aae195aef
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2024-01-17 20:44:52 +0700

    test

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
-console.log(process.env.API_URL);
+console.log(process.env.REACT_APP_API_URL);
 console.log("API_BASE_URL", API_BASE_URL);
 const API_ENDPOINTS = {
   PRODUCTS: "/products",
